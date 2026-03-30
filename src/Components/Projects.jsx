d80afff3ea2a28f79b406fa00af68c4950e5ba2f
poonamrana2016-creator <poonam.rana.2016@gmail.com>
2026-03-30 11:32:06 +0530

    updated link

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
               <div className="card-body">
                 <h5 className="card-title">Portfolio</h5>
                 <p className="card-text">A collection of my work focused on clean design and user-friendly web experiences.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <a href="https://portfoliopoonam.netlify.app/" className="btn btn-primary" target="_blank"  >Go somewhere</a>
               </div>
             </div>
           </div>
@@ -27,14 +27,14 @@ const Projects = () => {
               <div className="card-body">
                 <h5 className="card-title">Contact List Manager</h5>
                 <p className="card-text">A Contact List Manager is a web app to add, update, and delete contacts easily. It is built using HTML, CSS, JavaScript, Bootstrap, ReactJS, Node.js, and MongoDB.</p>
-                <a href="https://contact-listmanager.netlify.app/" className="btn btn-primary">Go somewhere</a>
+                <a href="https://contact-listmanager.netlify.app/"  target="_blank"  className="btn btn-primary">Go somewhere</a>
               </div>
             </div>
           </div>
 
           <div className="col-lg-4 col-md-6 col-sm-12">
             <div className="card contact_box project_box "  >
-              <img src="https://tunis-wordpress-dark.ibthemespro.com/wp-content/uploads/2023/09/project-3.jpg" className="card-img-top" alt="..." />
+              <img src="https://tunis-wordpress-dark.ibthemespro.com/wp-content/uploads/2023/09/project-3.jpg"  className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Amazon</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
